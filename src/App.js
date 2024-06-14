43976ca833c56c3d8fae9c837db0dbafe4a9e454
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-06-14 07:49:04 +0500

    fix: add home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LayoutPage from './components/Layout';
+import Homepage from './components/Homepage';
+import RegisterPage from './components/RegisterPage';
 
 function App() {
   return (
    <BrowserRouter>
-
+      <Routes>
+        <Route path='/' element={<LayoutPage />}>
+          <Route index element={<Homepage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/reducer' element={<RegisterPage />} />
+        </Route>
+      </Routes>
    </BrowserRouter>
   );
 }
